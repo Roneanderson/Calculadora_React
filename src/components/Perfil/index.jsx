6e66bd93e17becd23c0be5143c7efe875f56d6cd
Roneanderson <roneanderson496@gmail.com>
2024-07-27 00:52:47 -0300

    atualizado formulario IMC

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -1,4 +1,4 @@
-import {React,  useState } from "react"
+import { React, useState } from "react"
 
 
 function Formulario() {
@@ -6,10 +6,12 @@ function Formulario() {
     const [altura, setAltura] = useState(0);
     const [valorIMC, setValorIMC] = useState(0);
 
+    //função para armazena o valor
     function calculoImc(e) {
         setPeso(e.target.value);
     }
 
+    //função do botão calcular
     function calcularIMC() {
         let imc;
         imc = peso / altura / altura;
@@ -21,20 +23,20 @@ function Formulario() {
             <h1>Calculadora IMC</h1> <br />
 
             <form>
-                <label htmlFor="peso">Peso:</label> <br />
-                <input className="form-input" type="number" name="peso" value={peso} id="peso" onChange={calculoImc} />
+                <label className="link" htmlFor="peso">Peso:</label> <br />
+                <input className="dispo" required type="number" name="peso" value={peso} id="peso" onChange={calculoImc} />
 
                 <br /><br />
 
-                <label htmlFor="altura">Altura:</label> <br />
-                <input className="input" type="number" name="altura" value={altura} id="altura" onChange={e => setAltura(e.target.value)} />
+                <label className="link" htmlFor="altura">Altura:</label> <br />
+                <input className="dispo" required type="number" name="altura" value={altura} id="altura" onChange={e => setAltura(e.target.value)} />
 
                 <br /><br />
 
                 <input className="btn" type="button" value="Calcular IMC" onClick={calcularIMC} />
 
                 <br /><br />
-                <b>Seu imc</b>: {valorIMC.toFixed(2)}
+                <b>Seu imc é:</b> {valorIMC.toFixed(2)}
             </form>
         </div>
     )
